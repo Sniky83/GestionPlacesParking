ed66eb77df5f576261cebcd280f1d77e1f463e20
Sniky83 <sniky83@hotmail.fr>
2022-10-24 14:22:47 +0200

    Update script sql

--- Script Sql/INSERTS.sql
+++ Script Sql/INSERTS.sql
@@ -7,7 +7,7 @@ INSERT INTO ParkingSlot VALUES('P3')
 INSERT INTO ParkingSlot VALUES('P4')
 
 --PWD: apside_user
-INSERT INTO [User] VALUES ('user@apside-groupe.com', '3fce61ebb4f305664ddd45110f3f4f27e32e61b052609e0a027b0f64f08e0801')
+INSERT INTO [User] VALUES ('user@apside-groupe.com', '3fce61ebb4f305664ddd45110f3f4f27e32e61b052609e0a027b0f64f08e0801', 0)
 
 --PWD: apside_administrateur
-INSERT INTO [User] VALUES ('admin@apside-groupe.com', '7196165f31d788bc9e33eb64c47d88f5560c8d4a0806981eea986f13962ddf7f')
+INSERT INTO [User] VALUES ('admin@apside-groupe.com', '7196165f31d788bc9e33eb64c47d88f5560c8d4a0806981eea986f13962ddf7f', 1)
