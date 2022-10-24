ec6e2b7504e3fe1f6d0a2457b740bfdecd2b02e2
Sniky83 <sniky83@hotmail.fr>
2022-10-24 17:53:43 +0200

    Mise en place du service pour clean les réservations tous les vendredi a 11h

---
+++ CleanReservationsService/CleanReservationsService/CoreService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace CleanReservationsService
+{
+    public partial class CoreService : ServiceBase
+    {
+        public CoreService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            CleanReservations();
+        }
+
+        private void CleanReservations()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
