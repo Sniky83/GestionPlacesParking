61d499bc91e428d8b987c2279d55391a8d81f2d6
Sniky <sniky83@hotmail.fr>
2022-10-24 22:22:35 +0200

    Lancement automatique du service

--- CleanReservationsService/CleanReservationsService/ProjectInstaller.Designer.cs
+++ CleanReservationsService/CleanReservationsService/ProjectInstaller.Designer.cs
@@ -43,6 +43,7 @@
     " les réservations du projet Gestion des places de parking.";
             this.serviceInstaller1.DisplayName = "Mon Service qui nettoie les Réservations";
             this.serviceInstaller1.ServiceName = "CleanReservations";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
             // 
