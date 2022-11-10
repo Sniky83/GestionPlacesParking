09221bfefe02589f5b789ce4e27deeab594b7521
Sniky83 <sniky83@hotmail.fr>
2022-11-10 14:35:12 +0100

    Fin supression d'une réservation

--- GestionPlacesParking/Pages/Customs/Services/DeleteReservationService.cshtml.cs
+++ GestionPlacesParking/Pages/Customs/Services/DeleteReservationService.cshtml.cs
@@ -14,31 +14,30 @@ namespace GestionPlacesParking.Web.UI.Pages.Customs.Services
             _repository = repository;
         }
 
-        public IActionResult OnPost([FromBody] int ReservationId)
+        public IActionResult OnPost([FromBody] DeleteOneReservationDto DeleteOneReservationDto)
         {
             IActionResult result = Page();
+
+            string errorMessage = "Problème lors de la suppression de la réservation. Veuillez réessayer ultérieurement.";
 
             if (ModelState.IsValid)
             {
                 int? userId = HttpContext.Session.GetInt32("UserId");
                 int? isAdmin = HttpContext.Session.GetInt32("IsAdmin");
 
-                DeleteOneReservationDto deleteOneReservationDto = new DeleteOneReservationDto();
+                DeleteOneReservationDto.UserId = userId;
+                DeleteOneReservationDto.IsAdmin = (isAdmin == 1) ? true : false;
 
-                deleteOneReservationDto.ReservationId = ReservationId;
-                deleteOneReservationDto.UserId = userId;
-                deleteOneReservationDto.IsAdmin = (isAdmin == 1) ? true : false;
-
-                int rowAffected = _repository.DeleteReservation(deleteOneReservationDto);
+                int rowAffected = _repository.DeleteReservation(DeleteOneReservationDto);
 
                 if (rowAffected < 1)
                 {
-                    result = BadRequest(new { message = "Problème lors de la suppression de la réservation. Veuillez réessayer ultérieurement." });
+                    result = BadRequest(new { message = errorMessage });
                 }
             }
             else
             {
-                result = BadRequest(new { message = ModelState.Values.First().Errors.First().ErrorMessage });
+                result = BadRequest(new { message = errorMessage });
             }
 
             return result;
