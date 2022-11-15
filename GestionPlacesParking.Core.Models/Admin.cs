464dfa100e912880843d61a4344dcfec63d5cdef
Sniky83 <sniky83@hotmail.fr>
2022-11-15 10:57:56 +0100

    Ajout de la table Admin en BDD

---
+++ GestionPlacesParking.Core.Models/Admin.cs
@@ -0,0 +1,11 @@
+﻿namespace GestionPlacesParking.Core.Models
+{
+    /// <summary>
+    /// Référencie tous les ADMIN de l'application
+    /// </summary>
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+    }
+}
