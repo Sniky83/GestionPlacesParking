ff83b6d5f4339924187f770cb97f5d8c6c79801d
Louis HERVE <louis.herve@apside-groupe.com>
2022-11-15 10:02:11 +0000

    Ajout de la table ADMIN en BDD

---
+++ GestionPlacesParking.Core.Infrastructure/Migrations/20221115095618_AddAdminTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionPlacesParking.Core.Infrastructure.Migrations
+{
+    public partial class AddAdminTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+        }
+    }
+}
