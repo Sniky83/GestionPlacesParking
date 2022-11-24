70398853e91bc95f23e26adf9999dcb539627f23
msuchet <msuchet@apside-groupe.com>
2022-11-24 13:31:37 +0000

    Delete functionCreate.sh

--- DevOps/mssql/functionCreate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-createUserDB() 
-{
-    /opt/mssql-tools/bin/sqlcmd -U sa -P !@pside2022! -i /tmp/project/createDB.sql
-    /opt/mssql-tools/bin/sqlcmd -U sa -P !@pside2022! -i /tmp/project/createUser.sql
-}
