1762e82970ff63d68e629c09a9a4c261d58ff1cf
msuchet <msuchet@apside-groupe.com>
2023-01-31 15:58:02 +0000

    Update DevOps/builder/dockerfile, DevOps/builder/migration.sh

--- DevOps/builder/migration.sh
+++ DevOps/builder/migration.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/root/.dotnet/tools/dotnet-ef migrations add "AddTableUser"
+
 /root/.dotnet/tools/dotnet-ef database update --project=/src/myWebApp/GestionPlacesParking.Core.Infrastructure
