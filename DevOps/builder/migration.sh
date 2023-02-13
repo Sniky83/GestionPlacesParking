6843de1c524d8de1ccae4ef33db8f57d4fbcb3ef
msuchet <msuchet@apside-groupe.com>
2023-02-13 14:46:12 +0000

    Update .gitlab-ci.yml, DevOps/dockerfile, DevOps/builder/migration.sh Deleted DevOps/builder/dockerfile

--- DevOps/builder/migration.sh
+++ DevOps/builder/migration.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 /root/.dotnet/tools/dotnet-ef migrations add "AddTableUser" --project=/src/myWebApp/GestionPlacesParking.Core.Infrastructure
 /root/.dotnet/tools/dotnet-ef database update --project=/src/myWebApp/GestionPlacesParking.Core.Infrastructure
+dotnet /app/GestionPlacesParking.Web.UI.dll
