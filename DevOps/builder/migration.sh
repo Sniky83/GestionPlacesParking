2d7b2283b59bea8d5e5300ea9f90c21e964ee8e0
msuchet <msuchet@apside-groupe.com>
2023-02-14 08:45:43 +0000

    Update DevOps/builder/migration.sh

--- DevOps/builder/migration.sh
+++ DevOps/builder/migration.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 /root/.dotnet/tools/dotnet-ef migrations add "AddTableUser" --project=/src/myWebApp/GestionPlacesParking.Core.Infrastructure
 /root/.dotnet/tools/dotnet-ef database update --project=/src/myWebApp/GestionPlacesParking.Core.Infrastructure
+rm -f -R /src/myWebApp
+apt-get remove -y dotnet-sdk-7.0
 dotnet /app/GestionPlacesParking.Web.UI.dll
