7c69f153b033e8d29eaedbb8de1aeb888e6bf19f
msuchet <msuchet@apside-groupe.com>
2023-02-14 09:05:04 +0000

    Update .gitlab-ci.yml, DevOps/dockerfile, DevOps/builder/migration.sh

--- DevOps/builder/migration.sh
+++ DevOps/builder/migration.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+dotnet restore /src/myWebApp/GestionPlacesParking/GestionPlacesParking.Web.UI.csproj
+dotnet build /src/myWebApp/GestionPlacesParking/GestionPlacesParking.Web.UI.csproj -c Release -o /app/
+dotnet publish /src/myWebApp/GestionPlacesParking/GestionPlacesParking.Web.UI.csproj -c Release -o /app/
 /root/.dotnet/tools/dotnet-ef migrations add "AddTableUser" --project=/src/myWebApp/GestionPlacesParking.Core.Infrastructure
 /root/.dotnet/tools/dotnet-ef database update --project=/src/myWebApp/GestionPlacesParking.Core.Infrastructure
 rm -f -R /src/myWebApp
