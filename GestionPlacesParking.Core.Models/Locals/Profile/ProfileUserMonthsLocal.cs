20582796a2d2fe21486b5d97ec5df0c45e2e46db
Louis HERVE <louis.herve@apside-groupe.com>
2023-02-22 15:31:20 +0000

    Correction de bugs et refacto code

--- GestionPlacesParking.Core.Models/Locals/Profile/ProfileUserMonthsLocal.cs
+++ GestionPlacesParking.Core.Models/Locals/Profile/ProfileUserMonthsLocal.cs
@@ -3,7 +3,6 @@
     public class ProfileUserMonthsLocal
     {
         public int Mois { get; set; } = 0;
-        public string? MoisString { get; set; } = string.Empty;
         public int NbReservations { get; set; } = 0;
         public double MoyenneMois { get; set; } = 0;
     }
---
+++ GestionPlacesParking/ExtensionMethods/DIMethods.cs
@@ -0,0 +1,38 @@
+﻿using GestionPlacesParking.Core.Application.Repositories;
+using GestionPlacesParking.Core.Infrastructure.DataLayers;
+using GestionPlacesParking.Core.Interfaces.Infrastructures;
+using GestionPlacesParking.Core.Interfaces.Repositories;
+
+namespace SelfieAWookieAPI.ExtensionMethods
+{
+    public static class DIMethods
+    {
+        #region Public Methods
+        /// <summary>
+        /// Prepare l'injection de dépendance personnalisée.
+        /// </summary>
+        /// <param name="services"></param>
+        public static IServiceCollection AddInjections(this IServiceCollection Services)
+        {
+            // Injections de dépendances
+            Services.AddScoped<IUserDataLayer, SqlServerUserDataLayer>();
+            Services.AddScoped<IUserRepository, UserRepository>();
+
+            Services.AddScoped<IParkingSlotDataLayer, SqlServerParkingSlotDataLayer>();
+            Services.AddScoped<IParkingSlotRepository, ParkingSlotRepository>();
+
+            Services.AddScoped<IReservationDataLayer, SqlServerReservationDataLayer>();
+            Services.AddScoped<IReservationRepository, ReservationRepository>();
+
+            //Pas de dataLayer car en Local
+            Services.AddScoped<IDayLocalRepository, DayLocalRepository>();
+
+            //Utilise le dataLayer de Réservation
+            Services.AddScoped<IHistoryLocalRepository, HistoryLocalRepository>();
+            Services.AddScoped<IProfileLocalRepository, ProfileLocalRepository>();
+
+            return Services;
+        }
+        #endregion
+    }
+}
